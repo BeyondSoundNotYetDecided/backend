d840fadce7e392cef5ed4d41e2b7adb99ad7d425
uhyeonjin <uhyeonjin0619@gmail.com>
2026-01-22 16:50:31 +0900

    S14P11B104-145 전역 예외 처리기 및 API 공통 응답 포맷 구현

---
+++ src/main/java/com/example/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.backend.exception;
+
+import com.example.backend.common.ApiResponse;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@Hidden
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+// 전역 변수 처리기 클래스 입니다
+    
+    // 커스텀 예외 처리가 발생할 때 예외를 ApiResponse에 담아서 반환합니다
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ApiResponse<?>> handleCustomException(CustomException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus())
+                .body(ApiResponse.error(e.getErrorCode()));
+    }
+}

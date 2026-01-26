a35a0b23027c73adceff1bd7ed47dc524b06a88c
uhyeonjin <uhyeonjin0619@gmail.com>
2026-01-26 17:51:44 +0900

    S14P11B104-171 RabbitMQProducer (#5)

---
+++ src/main/java/com/example/backend/domain/ai/service/RabbitMQProducer.java
@@ -0,0 +1,26 @@
+package com.example.backend.domain.ai.service;
+
+import com.example.backend.domain.ai.dto.AnalysisRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class RabbitMQProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    // 인프라 중심 아키텍처입니다
+    // definition.js 파일의 설정을 기반으로 연결합니다
+    private final String EXCHANGE_NAME = "ai.exchange";
+    private final String ROUTING_KEY = "ai.job";
+
+    public void sendJob(Long feedbackId, String filePath, String script) {
+        AnalysisRequestDto message = new AnalysisRequestDto(filePath, feedbackId, script);
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, message);
+        System.out.println("[Spring] 메시지 전송 완료!");
+    }
+}

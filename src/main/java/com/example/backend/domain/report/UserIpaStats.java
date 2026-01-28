167cca90371229d17052efc20c6c54f3975bd39b
uhyunjin <uhyeonjin0619@gmail.com>
2026-01-28 20:14:10 +0900

    S14P11B104-196  User Ipa stats Entity

---
+++ src/main/java/com/example/backend/domain/report/UserIpaStats.java
@@ -0,0 +1,28 @@
+package com.example.backend.domain.report;
+
+import com.example.backend.domain.user.entity.User;
+import com.example.backend.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserIpaStats extends BaseTimeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ipa_id")
+    private Ipa ipa;
+
+    private Integer totalTryCount;
+    private Integer successCount;
+}
+

6ea509f55dc458343e8deecfdbe23cb94db93058
uhyunjin <uhyeonjin0619@gmail.com>
2026-01-28 20:13:54 +0900

    S14P11B104-196  Curriculum stats Entity

---
+++ src/main/java/com/example/backend/domain/curriculum/entity/CurriculumStats.java
@@ -0,0 +1,33 @@
+package com.example.backend.domain.curriculum.entity;
+
+import com.example.backend.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurriculumStats {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "curriculum_id")
+    private Curriculum curriculum;
+
+    private Integer tryCount; // 시도 횟수
+
+    @Column(nullable = false)
+    private Integer score;
+}
